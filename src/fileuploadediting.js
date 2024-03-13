d5ddb61d10224041a892bb26c8148d1e552045f5
Ajith AB <ajithanil@qburst.com>
2024-03-13 13:29:34 +0530

    getFileLinksFromChangeItem

--- src/fileuploadediting.js
+++ src/fileuploadediting.js
@@ -133,6 +133,7 @@ export default class FileUploadEditing extends Plugin {
 				console.log(entry, "entry.typeentry.typeentry.type");
 				if(entry.type == "attribute"){
 					const item = entry.range.start;
+					console.log("getFileLinksFromChangeItem", getFileLinksFromChangeItem( editor, item ));
 					if (item) {
 						for ( const file of getFileLinksFromChangeItem( editor, item ) ) {
 						if(entry?.attributeKey === "uploadId"){
