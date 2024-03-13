eaef18c64f7e4e6d4412b7664312d8375ed0a03a
Ajith AB <ajithanil@qburst.com>
2024-03-13 10:42:33 +0530

    flatRanges

--- src/fileuploadcommand.js
+++ src/fileuploadcommand.js
@@ -40,24 +40,19 @@ export default class FileUploadCommand extends Command {
 		
 		const range = selection.getFirstRange();
 		const selectedText = Array.from(range.getWalker()).map(item => item.data).join('');
+		console.log(selectedText, "selectedTextselectedText");
 		
 		editor.model.change(writer => {
 			const insertPosition = range.start;
 			const linkElement = writer.createElement('a');
 			writer.setAttribute('href', 'https://chat.openai.com/c/0b90c92a-81d2-4c2f-8aba-0e1f3e33ab06', linkElement);
 			
-			// Check if the range is non-flat
-			if (!range.isFlat) {
-				// If the range is non-flat, flatten it
-				const flatRanges = writer.flattenRange(range);
+			const flatRanges = writer.flattenRange(range);
+			console.log(flatRanges, "flatRangesflatRangesflatRanges");
 				// Wrap each flat range
 				flatRanges.forEach(flatRange => {
 					writer.wrap(linkElement, flatRange);
 				});
-			} else {
-				// If the range is already flat, simply wrap it
-				writer.wrap(linkElement, range);
-			}
 		});
 	
 		// const link = model.builder.create('link', { href: 'https://chat.openai.com/c/1106aad2-dff5-48c6-936e-6061b16e222e' });
