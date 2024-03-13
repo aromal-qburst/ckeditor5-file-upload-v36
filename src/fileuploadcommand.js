5739a39c05749deb307e622ec524921c9128739d
Ajith AB <ajithanil@qburst.com>
2024-03-13 11:20:13 +0530

    ranges

--- src/fileuploadcommand.js
+++ src/fileuploadcommand.js
@@ -1,6 +1,7 @@
 import FileRepository from '@ckeditor/ckeditor5-upload/src/filerepository';
 import Command from '@ckeditor/ckeditor5-core/src/command';
 import { insertFileLink, isFileAllowed } from './utils';
+import { findAttributeRange } from 'ckeditor5/src/typing';
 
 export default class FileUploadCommand extends Command {
 	/**
@@ -17,7 +18,7 @@ export default class FileUploadCommand extends Command {
 	 * @param {Object} options Options for the executed command.
 	 * @param {File|Array.<File>} options.file The file or an array of files to upload.
 	 */
-	execute( options ) {
+	execute( options, manualDecoratorIds = {} ) {
 		//const editor = this.editor;
 		// const model = editor.model;
 		// const fileRepository = editor.plugins.get( FileRepository );
@@ -32,20 +33,58 @@ export default class FileUploadCommand extends Command {
 		const editor = this.editor;
 		const model = editor.model;
 		const selection = model.document.selection;
+
+		const truthyManualDecorators = [];
+		const falsyManualDecorators = [];
+
+		for ( const name in manualDecoratorIds ) {
+			if ( manualDecoratorIds[ name ] ) {
+				truthyManualDecorators.push( name );
+			} else {
+				falsyManualDecorators.push( name );
+			}
+		}
 		
 		if (selection.isCollapsed) {
 			// Selection is collapsed (no text selected)
 			return;
 		}
 		
-		const range = selection.getFirstRange();
-		const selectedText = Array.from(range.getWalker()).map(item => item.data).join('');
-		
-		editor.model.change(writer => {
-			const insertPosition = range.start;
-			writer.remove(range);
-			writer.insertText(selectedText, { linkHref: 'https://chat.openai.com/c/1106aad2-dff5-48c6-936e-6061b16e222e' }, insertPosition);
-		});
+		// If selection has non-collapsed ranges, we change attribute on nodes inside those ranges
+				// omitting nodes where the `linkHref` attribute is disallowed.
+				const ranges = model.schema.getValidRanges( selection.getRanges(), 'linkHref' );
+
+				// But for the first, check whether the `linkHref` attribute is allowed on selected blocks (e.g. the "image" element).
+				const allowedRanges = [];
+
+				for ( const element of selection.getSelectedBlocks() ) {
+					if ( model.schema.checkAttribute( element, 'linkHref' ) ) {
+						allowedRanges.push( writer.createRangeOn( element ) );
+					}
+				}
+
+				// Ranges that accept the `linkHref` attribute. Since we will iterate over `allowedRanges`, let's clone it.
+				const rangesToUpdate = allowedRanges.slice();
+
+				// For all selection ranges we want to check whether given range is inside an element that accepts the `linkHref` attribute.
+				// If so, we don't want to propagate applying the attribute to its children.
+				for ( const range of ranges ) {
+					if ( this._isRangeToUpdate( range, allowedRanges ) ) {
+						rangesToUpdate.push( range );
+					}
+				}
+
+				for ( const range of rangesToUpdate ) {
+					writer.setAttribute( 'linkHref', 'https://chat.openai.com/c/1106aad2-dff5-48c6-936e-6061b16e222e', range );
+
+					truthyManualDecorators.forEach( item => {
+						writer.setAttribute( item, true, range );
+					} );
+
+					falsyManualDecorators.forEach( item => {
+						writer.removeAttribute( item, range );
+					} );
+				}
 	
 		// const link = model.builder.create('link', { href: 'https://chat.openai.com/c/1106aad2-dff5-48c6-936e-6061b16e222e' });
 		
@@ -53,6 +92,17 @@ export default class FileUploadCommand extends Command {
 		// 	// Replace selected text with the link
 		// 	writer.insertText(selectedText, link);
 		// });
+	}
+
+	_isRangeToUpdate( range, allowedRanges ) {
+		for ( const allowedRange of allowedRanges ) {
+			// A range is inside an element that will have the `linkHref` attribute. Do not modify its nodes.
+			if ( allowedRange.containsRange( range ) ) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 }
 
